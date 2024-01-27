678e7f5b569a704a282311075e86818dd60fc4ae
GeorgeOprea <oprea.george14@yahoo.com>
2024-01-27 18:28:11 +0200

    added assets, created basic structure of the app and every component file

--- bank_react/src/App.jsx
+++ bank_react/src/App.jsx
@@ -1,5 +1,46 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Billing from "./components/Billing";
+import Business from "./components/Business";
+import Button from "./components/Button";
+import CardDeal from "./components/CardDeal";
+import Clients from "./components/Clients";
+import Cta from "./components/CTA";
+import FeedbackCard from "./components/FeedbackCard";
+import Footer from "./components/Footer";
+import GetStarted from "./components/GetStarted";
+import Stats from "./components/Stats";
+import Testimonials from "./components/Testimonials";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="bg-primary w-full overflow-hid">
+      <div className="flex justify-center items-center sm:px-16 px-6">
+        <div className="xl:max-w-[1280px] w-full">
+          <Navbar />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-start bg-primary">
+        <div className="xl:max-w-[1280px] w-full">
+          <Hero />
+        </div>
+      </div>
+
+      <div className="flex justify-center items-start bg-primary sm:px-16 px-6">
+        <div className="xl:max-w-[1280px] w-full">
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <Cta />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
